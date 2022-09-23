51820f9cafc48cd7d3c762228674084ccba9579d
bebr <bobik33@gav.ru>
2022-09-23 16:02:26 +0500

    eratosthenes

---
+++ main.py
@@ -0,0 +1,20 @@
+def eratosthenes(n):
+    a = []
+    b = []
+
+    if n < 4:
+        return
+    for i in range(2, n + 1, 1):
+        a.append(i)
+    while a:
+        for i in a[1:]:
+            if i % a[0] == 0:
+                b.append(i)
+                a.remove(i)
+        a = a[1:]
+
+    for i in b:
+        print(i, end=" ")
+
+if __name__ == "__main__":
+    eratosthenes(50)
